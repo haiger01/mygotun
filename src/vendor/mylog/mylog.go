9cf1276557a38256d3e542cfdbea54edd9b10f84
mjw <jianwei.mo@oceanbluecloud.com>
2017-08-16 11:37:30 +0800

    save 20000 log info to per logfile

--- src/vendor/mylog/mylog.go
+++ src/vendor/mylog/mylog.go
@@ -5,22 +5,30 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"syscall"
-	"time"
 	"path"
 	"strings"
+	"sync"
+	"sync/atomic"
+	"syscall"
+	"time"
 )
 
 var logFile = flag.String("log.file", "", "save log file path")
-const(
+var logNum = flag.Int("log.num", 20000, " the loginfo number of log file")
+
+const (
 	LDEBUG = iota
-	LINFO	//1
+	LINFO  //1
 	LNOTICE
 	LWARNING
 	LERROR
 )
+
 var loglevel int
 var depth int = 2
+var MyLogInfoNum uint64 = 0
+var LogInfoThreshold uint64 = 0
+var logLock sync.Mutex
 
 var lf *os.File
 
@@ -38,31 +46,8 @@ func fileExist(filename string) bool {
 
 func InitLog(log_level int) {
 	loglevel = log_level
-	logfile := *logFile
-	var err error
-	log.SetFlags(log.Lshortfile | log.LstdFlags)
-	if logfile != "" {
-		if fileExist(logfile) {	
-			fmt.Println("log file already exist:", logfile)		
-			//t := time.Now().Format(layout)
-			t := time.Now()
-			year, month, day := t.Date()
-			filename :=  path.Base(logfile)
-			fileSuffix := path.Ext(filename) //获取文件后缀
-			filename_olny := strings.TrimSuffix(filename, fileSuffix)
-			logfile = path.Dir(logfile) + "/" + filename_olny +
-					fmt.Sprintf("_%d-%d-%d_%d-%d-%d", year, month, day, t.Hour(), t.Minute(), t.Second()) +
-					fileSuffix
-			fmt.Printf("create new log file name: %s\n", logfile)
-		}
-
-		lf, err = os.OpenFile(logfile, os.O_CREATE|os.O_RDWR, 0644)
-		if err != nil {
-			log.Fatalln("open log file error: ", err)
-		}
-		redirectStderr(lf)
-		log.SetOutput(lf)
-	}
+	LogInfoThreshold = uint64(*logNum)
+	createLogFile()
 }
 
 func Close() {
@@ -72,37 +57,80 @@ func Close() {
 }
 
 func init() {
-	
+
 }
 
-func Debug(format string, a ...interface{}){
-	if loglevel <= LDEBUG {
-		//log.Printf("[Debug] " + format, a...)
-		log.Output(depth, fmt.Sprintf("[Debug] " + format, a...))
+func createLogFile() {
+	logfile := *logFile
+	log.Printf("=======original log.file is : %s, try to create it, logNum=%d===========\n", logfile, *logNum)
+	var err error
+	log.SetFlags(log.Lshortfile | log.LstdFlags)
+	if logfile != "" {
+		if fileExist(logfile) {
+			log.Printf("log file %s already exist\n", logfile)
+			//t := time.Now().Format(layout)
+			t := time.Now()
+			year, month, day := t.Date()
+			filename := path.Base(logfile)
+			fileSuffix := path.Ext(filename) //获取文件后缀
+			filename_olny := strings.TrimSuffix(filename, fileSuffix)
+			logfile = path.Dir(logfile) + string(os.PathSeparator) + filename_olny +
+				fmt.Sprintf("_%d-%d-%d_%d-%d-%d", year, month, day, t.Hour(), t.Minute(), t.Second()) +
+				fileSuffix
+			log.Printf("generate new log file name: %s\n", logfile)
+		}
+		os.MkdirAll(path.Dir(logfile), 0644)
+		lf, err = os.OpenFile(logfile, os.O_CREATE|os.O_RDWR, 0644)
+		if err != nil {
+			log.Fatalln("open log file error: ", err)
+		}
+
+		log.Printf("create log file  %s success, now set log output to the file\n", logfile)
+		redirectStderr(lf)
+		log.SetOutput(lf)
 	}
-}
-func Info(format string, a ...interface{}){
-	if loglevel <= LINFO {
-		//log.Printf("[Info] " + format, a...)
-		log.Output(depth, fmt.Sprintf("[Info] " + format, a...))
-	}		
 }
 
-func Notice(format string, a ...interface{}){
-	if loglevel <= LNOTICE {
-		//log.Printf("[Notice] " + format, a...)
-		log.Output(depth, fmt.Sprintf("[Notice] " + format, a...))
+func NewLogFile() {
+	logLock.Lock()
+	//atomic.LoadUint64(&MyLogInfoNum)
+	if MyLogInfoNum <= LogInfoThreshold {
+		logLock.Unlock()
+		return
+	}
+	LogInfoThreshold += uint64(*logNum)
+	logLock.Unlock()
+
+	createLogFile()
+}
+
+func putToLog(level int, pre string, format string, a ...interface{}) {
+	if loglevel <= level {
+		pre_str := fmt.Sprintf("[%s %d] ", pre, MyLogInfoNum)
+		log.Output(depth, fmt.Sprintf(pre_str+format, a...))
+		atomic.AddUint64(&MyLogInfoNum, 1)
+		if MyLogInfoNum > LogInfoThreshold {
+			NewLogFile()
+		}
 	}
 }
-func Warning(format string, a ...interface{}){
-	if loglevel <= LWARNING {
-		//log.Printf("[Warning] " + format, a...)
-		log.Output(depth, fmt.Sprintf("[Warning] " + format, a...))
-	}		
+
+func Debug(format string, a ...interface{}) {
+	putToLog(LDEBUG, "Debug", format, a...)
 }
-func Error(format string, a ...interface{}){
-	if loglevel <= LERROR {
-		//log.Printf("[Error] " + format, a...)
-		log.Output(depth, fmt.Sprintf("[Error] " + format, a...))
-	}		
+
+func Info(format string, a ...interface{}) {
+	putToLog(LINFO, "Info", format, a...)
+}
+
+func Notice(format string, a ...interface{}) {
+	putToLog(LNOTICE, "Notice", format, a...)
+}
+
+func Warning(format string, a ...interface{}) {
+	putToLog(LWARNING, "Warning", format, a...)
+}
+
+func Error(format string, a ...interface{}) {
+	putToLog(LERROR, "Error", format, a...)
 }
